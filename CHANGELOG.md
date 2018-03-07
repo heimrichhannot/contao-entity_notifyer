a12b25163ab72711469c3abcdcda8876ecd71105
Martin Kunitzsch <m.kunitzsch@heimrich-hannot.de>
2018-03-07 10:40:37 +0100

    see cl @ 1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,12 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
+## [1.0.1] - 2018-03-07
+
+### Fixed
+- corrected names of methods
+
+
 ## [1.0.0] - 2018-03-05
 
 - initial commit
--- classes/backend/EntityNotifyerHelper.php
+++ classes/backend/EntityNotifyerHelper.php
@@ -37,7 +37,7 @@ class EntityNotifyerHelper extends Backend
         }
         
         \Controller::loadDataContainer($table);
-    
+        
         $fields = [];
         foreach ($GLOBALS['TL_DCA'][$table]['fields'] as $name => $data)
         {
@@ -45,7 +45,7 @@ class EntityNotifyerHelper extends Backend
             {
                 continue;
             }
-        
+            
             $fields[] = $name;
         }
         
@@ -62,43 +62,43 @@ class EntityNotifyerHelper extends Backend
     public static function getNotifications(\DataContainer $dc)
     {
         $options = [];
-    
+        
         if(null === ($notifications = Notification::findAll()))
         {
             return $options;
         }
         
         return $notifications->fetchEach('title');
-    
+        
     }
-
+    
     /**
-    * Return the "excludeFromEntityObserver" button
-    *
-    * @param array $row
-    * @param string $href
-    * @param string $label
-    * @param string $title
-    * @param string $icon
-    * @param string $attributes
-    *
-    * @return string
-    */
-    public function excludeFromEntityObserver($row, $href, $label, $title, $icon, $attributes)
+     * Return the "excludeFromEntityObserver" button
+     *
+     * @param array $row
+     * @param string $href
+     * @param string $label
+     * @param string $title
+     * @param string $icon
+     * @param string $attributes
+     *
+     * @return string
+     */
+    public function excludeFromEntityNotifyer($row, $href, $label, $title, $icon, $attributes)
     {
         $this->import('BackendUser','User');
         
-        if($row['excludeFromEntityObserver'])
+        if($row['excludeFromEntityNotifyer'])
         {
             $href  = 'key='.Entity::ENTITY_NOTIFYER_INCLUDE;
             $title = sprintf($GLOBALS['TL_LANG']['OBSERVER'][Entity::ENTITY_NOTIFYER_INCLUDE][1], $row['id']);
             $label = $GLOBALS['TL_LANG']['OBSERVER'][Entity::ENTITY_NOTIFYER_INCLUDE][0];
-        
+            
             return '<a href="' . $this->addToUrl($href . '&amp;id=' . $row['id']) . '" title="' . specialchars($title) . '"' . $attributes . '>' . Image::getHtml($icon, $label) . '</a> ';
         }
         
-        $icon = 'system/modules/entity_observer/assets/img/icon.png';
-    
+        $icon = 'system/modules/entity_notifyer/assets/img/icon.png';
+        
         return '<a href="' . $this->addToUrl($href . '&amp;id=' . $row['id']) . '" title="' . specialchars($title) . '"' . $attributes . '>' . Image::getHtml($icon, $label) . '</a> ';
     }
     
@@ -107,62 +107,62 @@ class EntityNotifyerHelper extends Backend
      *
      * @param \DataContainer $dc
      */
-    public function doExcludeFromEntityObserver(\DataContainer $dc)
+    public function doExcludeFromEntityNotifyer(\DataContainer $dc)
     {
         $redirect = str_replace('&key='.Entity::ENTITY_NOTIFYER_EXCLUDE, '', Environment::get('request'));
-    
+        
         $model = \Model::getClassFromTable($dc->table);
-    
+        
         if(null === ($entity = $model::findByPk($dc->id)))
         {
             Message::addError(sprintf($GLOBALS['TL_LANG']['MSC']['entityNotFound'], $dc->id));
             Controller::redirect($redirect);
         }
         
-        $entityExcluded = $entity->excludeFromEntityObserver;
+        $entityExcluded = $entity->excludeFromEntityNotifyer;
         
         if($entityExcluded)
         {
             Message::addError(sprintf($GLOBALS['TL_LANG']['MSC']['entityAlreadyExcluded'], $dc->id));
             Controller::redirect($redirect);
         }
-    
-        $entity->excludeFromEntityObserver = 1;
+        
+        $entity->excludeFromEntityNotifyer = 1;
         $entity->save();
-    
+        
         Message::addConfirmation(sprintf($GLOBALS['TL_LANG']['MSC']['entityExcludeSuccess'], $dc->table ?: $dc->id));
         Controller::redirect($redirect);
     }
-
-
+    
+    
     /**
      * set entity to be included in entity observer notification
      *
      * @param \DataContainer $dc
      */
-    public function includeEntityToEntityObserver(\DataContainer $dc)
+    public function includeEntityToEntityNotifyer(\DataContainer $dc)
     {
         $redirect = str_replace('&key='.Entity::ENTITY_NOTIFYER_INCLUDE, '', Environment::get('request'));
-    
+        
         $model = \Model::getClassFromTable($dc->table);
-    
+        
         if(null === ($entity = $model::findByPk($dc->id)))
         {
             Message::addError(sprintf($GLOBALS['TL_LANG']['MSC']['entityNotFound'], $dc->id));
             Controller::redirect($redirect);
         }
-    
-        $entityExcluded = $entity->excludeFromEntityObserver;
-    
+        
+        $entityExcluded = $entity->excludeFromEntityNotifyer;
+        
         if(!$entityExcluded)
         {
             Message::addError(sprintf($GLOBALS['TL_LANG']['MSC']['entityAlreadyIncluded'], $dc->id));
             Controller::redirect($redirect);
         }
-    
-        $entity->excludeFromEntityObserver = '';
+        
+        $entity->excludeFromEntityNotifyer = '';
         $entity->save();
-    
+        
         Message::addConfirmation(sprintf($GLOBALS['TL_LANG']['MSC']['entityIncludeSuccess'], $dc->table ?: $dc->id));
         Controller::redirect($redirect);
     }
